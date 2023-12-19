d611f41ee7fe278359c7f0767dd24c17ff57b6ce
Victor Dralle <woydowskidralle@gmail.com>
2023-12-19 12:33:03 +0100

    Started on navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import Login from "./screens/Login";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import MainScreen from "./screens/MainScreen";
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <Login/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 export default App;
