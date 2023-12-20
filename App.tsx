affb0514ff01af8d89e54fbbaed0b7a4ad6773e9
Victor Dralle <woydowskidralle@gmail.com>
2023-12-20 23:11:57 +0100

    Added a logout button just to test

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import MainScreen from "./screens/MainScreen";
+import auth from "@react-native-firebase/auth";
+import Login from "./screens/Login";
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Auth listener
+    const unsubscribe = auth().onAuthStateChanged(user => {
+      user ? setUser(user) : setUser(null);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={MainScreen} />
+        {user ? (
+          <Stack.Screen name="Home" component={MainScreen} />
+        ) : (
+          <Stack.Screen name="Login" component={Login} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   )
--- screens/MainScreen.jsx
+++ screens/MainScreen.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import auth from "@react-native-firebase/auth";
+
+
 
 const MainScreen = () => {
+    const logUserOut = () => {
+        auth().signOut().then(() => console.log("User signed out"));
+    }
+
     return (
         <View>
             <Text>Hello there!</Text>
+            <Button title="Sign out" onPress={logUserOut}>Sign Out</Button>
         </View>
     )
 }
